a70c63351709169f57b268a6b00c9e930933c00e
unknown <G00428429@atu.ie>
2025-02-28 13:26:58 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LsitComponent } from './lsit/lsit.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LsitComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'data-binding-app';
+export class AppComponent 
+{
+  count:number=0;
+  blHide:boolean = false;
+  myFunction()
+{
+  this.count++;
+}
+
+  onStarDBClick()
+  {
+    if(this.blHide)
+    {
+      this.blHide = false;
+    }
+    else
+    {
+      this.blHide = true;
+    }
+  }
 }
